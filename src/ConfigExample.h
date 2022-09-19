361490d4f7a2fd36bcc4d058b5796b400b9d4f80
spadr <enbunn1626@yahoo.co.jp>
2022-09-19 15:18:23 +0900

    add PlatformIO CI

---
+++ src/ConfigExample.h
@@ -0,0 +1,9 @@
+const char *ssid = "WiFi_SSID";
+const char *password = "WiFi_PASSWORD";
+const char *api_url = "Canaspad_project_in_supabase";
+const char *api_key = "anon_key";
+const char *api_username = "user@mail.com";
+const char *api_password = "password";
+const char *ntp_server = "ntp.nict.jp";
+const long gmt_offset_sec = 3600 * 9;
+const int daylight_offset_sec = 0;
